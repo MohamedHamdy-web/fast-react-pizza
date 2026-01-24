af1cbc3b0d44cd6343bfdeb55a18f7001dc7355f
Mohamed Hamdy <moh04591@gmail.com>
2026-01-24 04:59:17 +0200

    Add CreateOrder component and search functionality

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <div>
       <Link to="/">Fast React Pizaa Co.</Link>
+      <SearchOrder />
       <p>Your favorite pizza delivery service</p>
     </div>
   );
